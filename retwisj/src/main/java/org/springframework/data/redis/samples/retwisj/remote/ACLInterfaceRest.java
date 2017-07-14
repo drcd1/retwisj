375e717a70890f5755211a81568e56ff6afc34c5
drcd1 <duartercorreia@hotmail.com>
2017-07-14 01:48:14 +0100

    Added ACLInterfaceRest, added block links in network.jspf

---
+++ retwisj/src/main/java/org/springframework/data/redis/samples/retwisj/remote/ACLInterfaceRest.java
@@ -0,0 +1,25 @@
+package org.springframework.data.redis.samples.retwisj.remote;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.web.client.RestTemplate;
+
+public class ACLInterfaceRest implements ACLInterface{
+	
+	private final RestTemplate rest = new RestTemplate();
+	
+	public void block(String uid, String targetUid){
+		rest.getForObject("http://acl:8080/acl/block?id="+uid + "&tid=" + targetUid, String.class);
+	}
+
+	public void unblock(String uid, String targetUid){
+		rest.getForObject("http://acl:8080/acl/unblock?id="+uid + "&tid=" + targetUid, String.class);
+	}
+	
+	public Set<String> blocks(String uid){
+		return rest.getForObject("http://acl:8080/acl/blocks?id="+uid, Set.class);
+	}
+	
+	
+}
